2b066187f9b333de6239543da5ef44bb4848c08b
Tim Jenness <t.jenness@jach.hawaii.edu>
2011-01-03 13:38:10 -1000

    Standardise on GPL v3
    Previously the README and licence file indicated that GPL was being
    used but some of the source code indicated the Perl licence. Clarify
    that this code is all to be released under GPL and use version 3.

--- lib/Astro/FITS/Header/Item.pm
+++ lib/Astro/FITS/Header/Item.pm
@@ -895,8 +895,18 @@ Copyright (C) 2008-2009 Science and Technology Facilities Council.
 Copyright (C) 2001-2007 Particle Physics and Astronomy Research Council.
 All Rights Reserved.
 
-This program is free software; you can redistribute it and/or modify
-it under the same terms as Perl itself.
+This program is free software; you can redistribute it and/or modify it under
+the terms of the GNU General Public License as published by the Free Software
+Foundation; either version 3 of the License, or (at your option) any later
+version.
+
+This program is distributed in the hope that it will be useful,but WITHOUT ANY
+WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A
+PARTICULAR PURPOSE. See the GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License along with
+this program; if not, write to the Free Software Foundation, Inc., 59 Temple
+Place,Suite 330, Boston, MA  02111-1307, USA
 
 =head1 AUTHORS
 
@@ -905,5 +915,4 @@ Alasdair Allan E<lt>aa@astro.ex.ac.ukE<gt>
 
 =cut
 
-#     $Id$
 1;
